5cc5f157198ee9fa488c20dcb48d662ea3ed0849
matheussalencar <matheus.hsantos@mercadolivre.com>
2022-02-18 18:59:01 -0300

    Post request

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 	router.Run("localhost:5000")
 }
 
@@ -26,4 +27,14 @@ var albums = []album{
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
